a336f9869e35fbe97d59bbbdd171613ed285bc71
Mihir Rane <ranemihirk@gmail.com>
2025-01-24 16:17:35 +0530

    Added /test

---
+++ app/test/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { createBook } from "@/lib/redis";
+import { useState } from "react";
+
+export default function Create() {
+  const [error, setError] = useState("");
+
+  async function handleSubmit(formData) {
+    const result = await createBook(formData);
+
+    if (result?.error) {
+      setError(result.error);
+    }
+  }
+
+  return (
+    <main>
+      <form action={handleSubmit}>
+        <h2>Add a New Book</h2>
+        <input type="text" name="username" placeholder="username" />
+        <input type="password" name="password" placeholder="password" />
+        <button type="submit" className="btn">
+          Add Book
+        </button>
+        {error && <div className="error">{error}</div>}
+      </form>
+    </main>
+  );
+}
