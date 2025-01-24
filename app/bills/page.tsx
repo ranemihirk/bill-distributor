9e780c1c05e539b8cedbeabede64a7b76bf3d253
Mihir Rane <ranemihirk@gmail.com>
2025-01-24 17:40:37 +0530

    Resolved the issue

--- app/bills/page.tsx
+++ app/bills/page.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 export const dynamic = "force-dynamic";
-import BillsListPage from "@/components/Bills/BillsList";
+import BillsListPage from "@/components/Bills/BillsListComponent";
 
 export default async function BillsPage() {
   // const data = await fetchAPI();
